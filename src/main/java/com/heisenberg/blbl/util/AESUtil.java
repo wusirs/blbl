f7f7d9c94d592fd929173a1bea7d4189b5a446d5
wusirs <2411851860@qq.com>
2024-02-01 11:35:29 +0800

    加解密

--- src/main/java/com/heisenberg/blbl/util/AESUtil.java
+++ src/main/java/com/heisenberg/blbl/util/AESUtil.java
@@ -9,11 +9,9 @@ import org.springframework.util.Base64Utils;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.Collections;
 
 
 /**
@@ -49,7 +47,7 @@ public class AESUtil {
      * AES加密
      * @param content 待加密内容
      * @param aesKey  密码
-     * @return
+     * @return {@link String}
      */
     public static String encrypt(String content, String aesKey){
         if(StringUtils.isBlank(content)){
@@ -70,7 +68,7 @@ public class AESUtil {
                 IvParameterSpec ivParameterSpec = null;
                 cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivParameterSpec);
                 //根据待加密内容生成字节数组
-                byte[] encrypted = cipher.doFinal(content.getBytes(ENCODING));
+                byte[] encrypted = cipher.doFinal(content.getBytes(StandardCharsets.UTF_8));
                 //返回base64字符串
                 return Base64Utils.encodeToString(encrypted);
             } catch (Exception e) {
@@ -89,7 +87,7 @@ public class AESUtil {
      *
      * @param content 待解密内容
      * @param aesKey  密码
-     * @return
+     * @return {@link String}
      */
     public static String decrypt(String content, String aesKey){
         if(StringUtils.isBlank(content)){
@@ -120,8 +118,7 @@ public class AESUtil {
                 //将字节数组转成字符串
                 return new String(decrypted, ENCODING);
             } catch (Exception e) {
-                LOGGER.info("AES decrypt exception:" + e.getMessage());
-                throw new RuntimeException(e);
+                throw new IllegalStateException(e);
             }
 
         }else {
@@ -135,9 +132,9 @@ public class AESUtil {
      *
      * @param content 待加密内容
      * @param aesKey  密码
-     * @return
+     * @return {@link String}
      */
-    public static String encryptCBC(String content, String aesKey){
+    public static String encryptCBC(String content, String aesKey) {
         if(StringUtils.isBlank(content)){
             LOGGER.info("AES_CBC encrypt: the content is null!");
             return null;
@@ -146,24 +143,22 @@ public class AESUtil {
         if(StringUtils.isNotBlank(aesKey) && aesKey.length() == 16){
             try {
                 //对密码进行编码
-                byte[] bytes = aesKey.getBytes(ENCODING);
+                byte[] bytes = aesKey.getBytes(StandardCharsets.UTF_8);
                 //设置加密算法，生成秘钥
                 SecretKeySpec skeySpec = new SecretKeySpec(bytes, AES_ALGORITHM);
                 // "算法/模式/补码方式"
                 Cipher cipher = Cipher.getInstance(CIPHER_CBC_PADDING);
                 //偏移
-                IvParameterSpec iv = new IvParameterSpec(IV_SEED.getBytes(ENCODING));
+                IvParameterSpec iv = new IvParameterSpec(IV_SEED.getBytes(StandardCharsets.UTF_8));
                 //选择加密
                 cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
                 //根据待加密内容生成字节数组
-                byte[] encrypted = cipher.doFinal(content.getBytes(ENCODING));
+                byte[] encrypted = cipher.doFinal(content.getBytes(StandardCharsets.UTF_8));
                 //返回base64字符串
                 return Base64Utils.encodeToString(encrypted);
             } catch (Exception e) {
-                LOGGER.info("AES_CBC encrypt exception:" + e.getMessage());
-                throw new RuntimeException(e);
+                throw new IllegalStateException(e);
             }
-
         }else {
             LOGGER.info("AES_CBC encrypt: the aesKey is null or error!");
             return null;
@@ -175,9 +170,9 @@ public class AESUtil {
      *
      * @param content 待解密内容
      * @param aesKey  密码
-     * @return
+     * @return {@link String}
      */
-    public static String decryptCBC(String content, String aesKey){
+    public static String decryptCBC(String content, String aesKey) {
         if(StringUtils.isBlank(content)){
             LOGGER.info("AES_CBC decrypt: the content is null!");
             return null;
@@ -186,11 +181,11 @@ public class AESUtil {
         if(StringUtils.isNotBlank(aesKey) && aesKey.length() == 16){
             try {
                 //对密码进行编码
-                byte[] bytes = aesKey.getBytes(ENCODING);
+                byte[] bytes = aesKey.getBytes(StandardCharsets.UTF_8);
                 //设置解密算法，生成秘钥
                 SecretKeySpec skeySpec = new SecretKeySpec(bytes, AES_ALGORITHM);
                 //偏移
-                IvParameterSpec iv = new IvParameterSpec(IV_SEED.getBytes(ENCODING));
+                IvParameterSpec iv = new IvParameterSpec(IV_SEED.getBytes(StandardCharsets.UTF_8));
                 // "算法/模式/补码方式"
                 Cipher cipher = Cipher.getInstance(CIPHER_CBC_PADDING);
                 //选择解密
@@ -202,53 +197,47 @@ public class AESUtil {
                 //根据待解密内容进行解密
                 byte[] decrypted = cipher.doFinal(decodeBase64);
                 //将字节数组转成字符串
-                return new String(decrypted, ENCODING);
+                return new String(decrypted, StandardCharsets.UTF_8);
             } catch (Exception e) {
-                LOGGER.info("AES_CBC decrypt exception:" + e.getMessage());
-                throw new RuntimeException(e);
+                throw new IllegalArgumentException(e);
             }
-
         }else {
             LOGGER.info("AES_CBC decrypt: the aesKey is null or error!");
             return null;
         }
     }
 
-    public static void main(String[] args) throws UnsupportedEncodingException {
+    public static void main(String[] args) {
         // AES支持三种长度的密钥：128位、192位、256位。
         // 代码中这种就是128位的加密密钥，16字节 * 8位/字节 = 128位。
 
 
         String random = RandomStringUtils.random(16, "abcdefghijklmnopqrstuvwxyz1234567890");
-
+        LOGGER.info("random: {}", random);
         random  = "sQPoC/1do9BZMkg8I5c09A==";
         String aesResult = "iTgNXRqU80YZ52+iV7Ew2w==";
+        LOGGER.info("aesResult: {}", aesResult);
 
-        System.out.println("随机key:" + random);
-        System.out.println(Base64.getDecoder().decode(random).length);
-        byte[] a1 = "abc".getBytes("UTF-8");
-        System.out.println(Arrays.toString(a1));
+        LOGGER.info("随机key:{}", random);
+        LOGGER.info("length:{}",Base64.getDecoder().decode(random).length);
+        byte[] a1 = "abc".getBytes(StandardCharsets.UTF_8);
+        LOGGER.info(Arrays.toString(a1));
 
-        System.out.println("---------加密---------");
+        LOGGER.info("---------加密---------");
         aesResult = encrypt("Hello", random);
-        System.out.println("aes加密结果:" + aesResult);
-        System.out.println();
+        LOGGER.info("aes加密结果:{}", aesResult);
 
 
-        System.out.println("---------解密---------");
+        LOGGER.info("---------解密---------");
         String decrypt = decrypt(aesResult, random);
-        System.out.println("aes解密结果:" + decrypt);
-        System.out.println();
-//
-//
-        System.out.println("--------AES_CBC加密解密---------");
+        LOGGER.info("aes解密结果:{}", decrypt);
+
+        LOGGER.info("--------AES_CBC加密解密---------");
         aesResult = encryptCBC("Hello", random);
-        System.out.println("aes_cbc加密结果:" + aesResult);
-//        System.out.println();
-//
-        System.out.println("---------解密CBC---------");
+        LOGGER.info("aes_cbc加密结果:{}", aesResult);
+
+        LOGGER.info("---------解密CBC---------");
         String cbcDecrypt = decryptCBC(aesResult, random);
-        System.out.println("aes解密结果:" + cbcDecrypt);
-        System.out.println();
+        LOGGER.info("aes解密结果:{}", cbcDecrypt);
     }
 }
--- src/main/java/com/heisenberg/blbl/util/AesDemo.java
+++ src/main/java/com/heisenberg/blbl/util/AesDemo.java
@@ -1,8 +1,14 @@
 package com.heisenberg.blbl.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.security.NoSuchAlgorithmException;
 
 public class AesDemo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AesDemo.class);
 
     public static void main(String[] args) throws Exception {
         jjmTest();
@@ -11,16 +17,16 @@ public class AesDemo {
     public static void jjmTest() throws Exception {
         String secretKey = CryptoUtils.generateSymmetricKey(CryptoUtils.Algorithm.Encryption.AES_ECB_PKCS5);
         String cipherText = CryptoUtils.encryptSymmetrically(secretKey, null, "test", CryptoUtils.Algorithm.Encryption.AES_ECB_PKCS5);
-        System.out.println("生成的密钥为：" + secretKey);
-        System.out.println("加密后的密文为：" + cipherText);
-        System.out.println("解密后的明文为：" + CryptoUtils.decryptSymmetrically(secretKey, null, cipherText, CryptoUtils.Algorithm.Encryption.AES_ECB_PKCS5));
+        LOGGER.info("生成的密钥为：{}", secretKey);
+        LOGGER.info("加密后的密文为：{}", cipherText);
+        LOGGER.info("解密后的明文为：{}", CryptoUtils.decryptSymmetrically(secretKey, null, cipherText, CryptoUtils.Algorithm.Encryption.AES_ECB_PKCS5));
     }
 
 
     public static void decrypt() throws Exception {
         String secretKey = "sQPoC/1do9BZMkg8I5c09A==";
         String cipherText = "iTgNXRqU80YZ52+iV7Ew2w==";
-        System.out.println("解密后的明文为：" + CryptoUtils.decryptSymmetrically(secretKey, null, cipherText, CryptoUtils.Algorithm.Encryption.AES_ECB_PKCS5));
+        LOGGER.info("解密后的明文为：{}", CryptoUtils.decryptSymmetrically(secretKey, null, cipherText, CryptoUtils.Algorithm.Encryption.AES_ECB_PKCS5));
     }
 }
 
