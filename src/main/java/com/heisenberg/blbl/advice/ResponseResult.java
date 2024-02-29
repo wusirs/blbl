311f364777b6c4c8dfc5769c6747c46a0d40c345
wusirs <2411851860@qq.com>
2024-02-29 20:00:46 +0800

    包装返回值

--- src/main/java/com/heisenberg/blbl/advice/ResponseResult.java
+++ src/main/java/com/heisenberg/blbl/advice/ResponseResult.java
@@ -34,7 +34,7 @@ public class ResponseResult implements ResponseBodyAdvice<Object> {
 
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        if (body instanceof Object) {
+        if (body instanceof Result) {
             // 提供一定的灵活度，如果body已经被包装了，就不进行包装
             return body;
         }
