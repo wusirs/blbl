0c6430ea7ac6d5bad91e320df5b98500a8e1ad25
wusirs <2411851860@qq.com>
2024-03-29 11:30:54 +0800

    sql

--- src/main/resources/sql/tablespace.sql
+++ src/main/resources/sql/tablespace.sql
@@ -1,6 +1,7 @@
 # 1、创建数据库
 #指定utf8字符创建
 create database blbl character set utf8;
+create database common character set utf8;
 
 # 2、创建用户
 #创建用户userwordpress，仅限本机访问，密码123456
@@ -13,6 +14,7 @@ create user 'WL'@'%' identified by '1234zxcv';
 # 3、授权用户
 #将数据库wordpress下的所有（*）授权给userwordpress
 grant all privileges on blbl.* to 'WL'@'%';
+grant all privileges on common.* to 'WL'@'%';
 #最后刷新权限
 flush privileges;
 
